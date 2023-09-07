ee5ac9824dd23ec359707b698f5a24436918bfdf
Daniel Londoño <daniel.esloh@gmail.com>
2023-09-06 21:35:14 -0500

    Added link to titles

--- client/src/components/AppRoutes.jsx
+++ client/src/components/AppRoutes.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+
 import PostsList from "../features/posts/PostsList";
 import PostDetails from "../features/posts/PostDetails";
 import NewPostForm from "../features/posts/NewPostForm";
