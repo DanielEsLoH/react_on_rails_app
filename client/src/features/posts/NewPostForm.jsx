b78f23f11a3d4334b7ab2ff6374b82d82285de74
Daniel Lonodoño <daniel.esloh@gmail.com>
2023-11-20 14:27:09 -0500

    Complete coverage for newPostForm

--- client/src/features/posts/NewPostForm.jsx
+++ client/src/features/posts/NewPostForm.jsx
@@ -1,54 +1,54 @@
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
-import { createPost } from "../../services/postService"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { createPost } from "../../services/postService";
 
 function NewPostForm() {
-    const [title, setTitle] = useState("")
-    const [body, setBody] = useState("")
-    const navigate = useNavigate()
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        const postData = { title, body }
+    const postData = { title, body };
 
-        try {
-            const response = await createPost(postData)
-            navigate(`/posts/${response.id}`)
-        } catch (e) {
-            console.log("Failed to create post: ", e)
-        }
+    try {
+      const response = await createPost(postData);
+      navigate(`/posts/${response.id}`);
+    } catch (e) {
+      console.error("Failed to create post: ", e);
     }
+  };
 
-    return (
+  return (
+    <div>
+      <h2>Create a New Post</h2>
+      <form onSubmit={handleSubmit}>
         <div>
-            <h1>New Post Form</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="titleInput">Title:</label>
-                    <input
-                        id="titleInput"
-                        type="text"
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="bodyInput">Body:</label>
-                    <textarea
-                        id="bodyInput"
-                        value={body}
-                        onChange={(e) => setBody(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <button type="submit">Create Post</button>
-                </div>
-            </form>
+          <label htmlFor="titleInput">Title:</label>
+          <input
+            id="titleInput"
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
         </div>
-    )
+        <div>
+          <label htmlFor="bodyInput">Body:</label>
+          <textarea
+            id="bodyInput"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <button type="submit">Create Post</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default NewPostForm
+export default NewPostForm;
