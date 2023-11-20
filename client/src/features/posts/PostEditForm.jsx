56b91163dc87497ed8e6e046524c62d82b1bae29
Daniel Lonodoño <daniel.esloh@gmail.com>
2023-11-20 14:37:45 -0500

    Add Coverage to PostEditform

--- client/src/features/posts/PostEditForm.jsx
+++ client/src/features/posts/PostEditForm.jsx
@@ -1,76 +1,78 @@
-import { useEffect, useState } from "react"
-import { useParams, useNavigate } from "react-router-dom"
-import { fetchPost, updatePost } from "../../services/postService"
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { fetchPost, updatePost } from "../../services/postService";
 
 function EditPostForm() {
-    const [post, setPost] = useState(null)
-    const { id } = useParams()
-    const [, setLoading] = useState(true)
-    const [, setError] = useState(null)
-    const navigate = useNavigate()
+  const [post, setPost] = useState(null);
+  const { id } = useParams();
+  const [, setLoading] = useState(true);
+  const [, setError] = useState(null);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        // Fetch the current post by id
-        const fetchCurrentPost = async () => {
-            try {
-                const json = await fetchPost(id)
-                setPost(json)
-            } catch (e) {
-                setError(e)
-            } finally {
-                setLoading(false)
-            }
-        }
-        fetchCurrentPost()
-    }, [id])
+  useEffect(() => {
+    // Fetch the current post by id
+    const fetchCurrentPost = async () => {
+      try {
+        const json = await fetchPost(id);
+        setPost(json);
+      } catch (e) {
+        console.error("Failed to fetch the post: ", e);
+        setError(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCurrentPost();
+  }, [id]);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        
-        const updatedPost = {
-            title: post.title,
-            body: post.body
-        }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        try {
-            const response = await updatePost(id, updatedPost)
-            navigate(`/posts/${response.id}`)
-        } catch (e) {
-            console.error("Failed to update post: ", e)
-        }
+    const updatedPost = {
+      title: post.title,
+      body: post.body,
+    };
+
+    try {
+      await updatePost(id, updatedPost);
+      navigate(`/posts/${id}`);
+    } catch (e) {
+      console.error("Failed to update the post: ", e);
     }
+  };
 
-    if (!post) return <h2>Loading...</h2>
+  if (!post) return <h2>Loading...</h2>;
 
-    return (
+  return (
+    <div>
+      <h2>Edit Post</h2>
+
+      <form onSubmit={handleSubmit}>
         <div>
-            <h2>Edit Post Form here</h2>
-
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="post-title">Title</label>
-                    <br />
-                    <input 
-                        type="text"
-                        id="post-title"
-                        value={post.title} 
-                        onChange={(e) => setPost({...post, title: e.target.value})} 
-                    />
-                </div>
-                <div>
-                    <label htmlFor="post-body">Body</label>
-                    <br />
-                    <textarea 
-                        id="post-body"
-                        value={post.body}
-                        onChange={(e) => setPost({...post, body: e.target.value})} 
-                    />
-                </div>
-                <div>
-                    <button type="submit">Save</button>
-                </div>
-            </form>
+          <label htmlFor="post-title">Title</label>
+          <br />
+          <input
+            type="text"
+            id="post-title"
+            value={post.title}
+            onChange={(e) => setPost({ ...post, title: e.target.value })}
+          />
         </div>
-    )
+        <div>
+          <label htmlFor="post-body">Body</label>
+          <br />
+          <textarea
+            id="post-body"
+            value={post.body}
+            onChange={(e) => setPost({ ...post, body: e.target.value })}
+          />
+        </div>
+        <div>
+          <button type="submit">Save</button>
+        </div>
+      </form>
+    </div>
+  );
 }
-export default EditPostForm
+
+export default EditPostForm;
